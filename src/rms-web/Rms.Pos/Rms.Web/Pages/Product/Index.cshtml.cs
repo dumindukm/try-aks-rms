5f8372a2fa29ca723c73a23efea45d6f2879ba05
Dumindu <dumindukm@gmail.com>
2022-03-07 21:17:20 +0530

    include pipeline for aks

--- src/rms-web/Rms.Pos/Rms.Web/Pages/Product/Index.cshtml.cs
+++ src/rms-web/Rms.Pos/Rms.Web/Pages/Product/Index.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace Rms.Web.Pages.Product
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration configuration;
+        public MySettings settings;
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public void OnGet()
         {
+            settings =this.configuration.Get<MySettings>();
         }
+    }
+
+    public class MySettings
+    {
+        public string GatewayBaseUrl { get; set; }
+        public string SiteText { get; set; }
     }
 }
